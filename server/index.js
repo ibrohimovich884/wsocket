4d92aa9dbc85033597127560baf13ed6bf21fe0c
ibrohimovich884 <bkbekmirzayev@gmail.com>
2025-08-29 10:19:24 +0500

    1

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ const collection = client.db("social").collection("passwords")
 
 app.use(express.json())
 app.use(cors({
+  
   origin: "http://localhost:5173",
   methods: ["GET", "POST"],
   allowedHeaders: ["Content-Type"],
